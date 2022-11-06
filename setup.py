877bea88a1155c2c9caea0f6fe2a8b52e68f9fef
leffw <93302536+leffw@users.noreply.github.com>
2022-11-06 14:25:23 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name="lnd", version="0.1.0", py_modules=["lnd"], install_requires=["requests"])
+setup(name="lnd", version="0.1.0", py_modules=["lnd"], install_requires=["requests", "cachetools"])
